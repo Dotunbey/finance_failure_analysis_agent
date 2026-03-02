525fdd2afd26de6486c1760210a5744fc7ffd80b
Aina Adoption Oluwasomidotun <ainaadoption@gmail.com>
2026-03-02 08:24:49 +0100

    Create settings.py

---
+++ src/settings.py
@@ -0,0 +1,13 @@
+
+from pydantic import BaseSettings
+
+class Settings(BaseSettings):
+    """Settings for the application."""
+    hf_model_name: str = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
+    max_new_tokens: int = 128
+    log_level: str = "INFO"
+
+    class Config:
+        """Pydantic configuration class."""
+        env_file = ".env"
+
