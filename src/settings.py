bc52ec77a6711a00e0e6a09927d0924964c925a6
Aina Adoption Oluwasomidotun <ainaadoption@gmail.com>
2026-03-07 15:30:46 +0100

    Add api_host and api_port to settings

--- src/settings.py
+++ src/settings.py
@@ -1,4 +1,4 @@
-
+#!src/settings.py
 from pydantic import BaseSettings
 
 class Settings(BaseSettings):
@@ -6,8 +6,9 @@ class Settings(BaseSettings):
     hf_model_name: str = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
     max_new_tokens: int = 128
     log_level: str = "INFO"
+    api_host: str = "0.0.0.0"
+    api_port: int = 8000
 
     class Config:
         """Pydantic configuration class."""
         env_file = ".env"
-
